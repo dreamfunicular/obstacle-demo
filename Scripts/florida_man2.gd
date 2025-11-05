69d1388a9a634a50d4983d3efc8cd5d8b229ef7f
bbccAri <boxybirdmc@gmail.com>
2025-11-05 14:33:00 -0500

    add coyote time and fix being able to jump extra; add death zone

--- Scripts/florida_man2.gd
+++ Scripts/florida_man2.gd
@@ -3,6 +3,7 @@ class_name FloridaMan
 
 @onready var sprite = $AnimatedSprite2D as AnimatedSprite2D
 @onready var anim = $AnimationPlayer as AnimationPlayer
+@onready var coyoteTimer = $CoyoteTimer as Timer
 
 @export_category("Movement Parameters")
 @export var Jump_Peak_Time: float = .15
@@ -14,12 +15,29 @@ var Jump_Velocity: float = 500.0
 
 @export var DefaultJumpLimit: int = 1
 var jumps: int = 0
+var wasGrounded: bool = false
+@export var CoyoteTime: float = 0.1
+var canCoyote: bool = false
 
 var Jump_Gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var Fall_Gravity: float = 980.0
 
+var SpawnPoint: Vector2 = Vector2(0, 0)
+
 func _ready() -> void:
 	Calculate_Movement_Parameters()
+	SpawnPoint = position
+	GameManager.connect("Reset", resetFunc)
+
+var resetFunc = func Reset()->void:
+	position = SpawnPoint
+
+func SetSpawn(point: Vector2)->void:
+	SpawnPoint = point
+
+func Die()->void:
+	#Death sound
+	GameManager.emit_signal("Reset")
 
 func Calculate_Movement_Parameters()->void:
 	Jump_Gravity = (2*Jump_Height)/pow(Jump_Peak_Time,2)
@@ -34,16 +52,24 @@ func _physics_process(delta: float) -> void:
 			velocity.y += Jump_Gravity * delta
 		else:
 			velocity.y += Fall_Gravity * delta
+		if wasGrounded and jumps > 0:
+			canCoyote = true
+			jumps -= 1
+			coyoteTimer.start(CoyoteTime)
+		wasGrounded = false
 
 	if is_on_floor():
 		jumps = DefaultJumpLimit
+		wasGrounded = true
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jumps > 0:
+	if Input.is_action_just_pressed("jump") and (jumps > 0 or canCoyote):
 		if velocity.y > 0:
 			velocity.y = 0
 		velocity.y -= Jump_Velocity
-		jumps -= 1
+		if jumps > 0:
+			jumps -= 1
+		canCoyote = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -54,3 +80,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, Speed)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout() -> void:
+	canCoyote = false
---
+++ Scripts/game_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal Reset
+signal SetSpawn
